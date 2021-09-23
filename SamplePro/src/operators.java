e8dedff04d954f90bb10182a6fc3ae471fbdb3e5
S.Vaishnavi <S.Vaishnavi@DESKTOP-1Q677PP>
2021-09-23 10:49:28 +0530

    SampleProgram

---
+++ SamplePro/src/operators.java
@@ -0,0 +1,25 @@
+
+public class operators {
+
+	public static void main(String[] args) {
+		
+		// declare variables and initiate values
+		int val1=10;
+		int val2=20;
+		
+		// Arithmetic Operators
+		int addresult=val1+val2;
+		int subresult=val1-val2;
+		int mulresult=val1*val2;
+		int divresult=val1/val2;
+		int modresult=val1%val2;
+		System.out.println("add:" +addresult);
+		System.out.println("sub:" +subresult);
+		System.out.println("mul:" +mulresult);
+		System.out.println("div:" +divresult);
+		System.out.println("mod:" +modresult);
+		
+
+	}
+
+}
